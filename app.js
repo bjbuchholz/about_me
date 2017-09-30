9d581ecc9898dc5ba2b7767bbc26b847e04aabb0
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2017-09-30 00:23:29 -0700

    added CSS code

--- app.js
+++ app.js
@@ -11,45 +11,45 @@ alert('Hi ' + user + '! I am so glad you want to play my game.');
 
 var answer = prompt('First question is: Do you think I am a car guy? Please respond yes or no.').toLowerCase();
 console.log(answer);
-if(answer === 'yes' || answer === 'Y') {
-  alert('You guessed right ' + user + ', I have owned a vehicle from 13 different manufacturers, and over 20 different cars, so cars are my "JAM" ');
+if(answer === 'yes' || answer === 'y') {
+  alert('You guessd right ' + user + ', I have owned a vehicle from 13 different manufacturers, and owned 20 different cars, they are my "JAM" ');
   userPoints++;
 } else {
-  alert('I am a big car guy, I have had over 20 of them from 13 different manufacturers. “Vroom Vroom” ');
+  alert('Actually, I am a big car guy, I have had over 20 of them from 13 different manufacturers. “Vroom Vroom” ');
 } alert('You have ' + userPoints + ' points.');
 
-var answer2 = prompt('So ' + user + ', now that you know I "Love" to get behind the wheel, let\'s see if you can guess one of my favorite cars that I would love to own. Do you think my favorite car is American made? Please respond Yes or No. ').toLowerCase();
+var answer2 = prompt('So ' + user + ', now that you know I "Love" to get behind the wheel, let\'s see if you can guess one of my favorite cars that I hope to own. Do you think my favorite car is American made? Please respond Yes or No. ').toLowerCase();
 console.log(answer2);
-if(answer2 === 'yes' || answer2 === 'Y') {
-  alert('While I do think it would be fun to shred some tires in a Corvette, it\'s not the one I have in mind. ');
+if(answer2 === 'yes' || answer2 === 'y') {
+  alert('Not quite, while I do think it would be fun to shred the tires off a Corvette, it\'s not the one I have in mind. ');
 } else {
   userPoints++;
-  alert('That\’s correct' + user + ', I do think American cars are great but the one I am thinking about is a little more exotic.');
+  alert('That\’s correct ' + user + ', while I do think American cars are great, I am more a foreign car guy. ');
 } alert('You have ' + userPoints + ' points.');
 
-var answer3 = prompt('So big American muscle isn\'t for me, but I would take a free Corvette if someone offered it. Do you think it is Asian built ' + user + '? Yes or No.').toLowerCase();
+var answer3 = prompt('So know that you know big American muscle isn\'t for me. Do you think it is an Asian built car ' + user + '? Yes or No.').toLowerCase();
 console.log(answer3);
-if( answer3 === 'yes' || answer3 === 'Y') {
-  alert('While I have owned quite a few Asian designed cars, I really love the design and performance coming from our neighbors across the pond.');
+if( answer3 === 'yes' || answer3 === 'y') {
+  alert('While I have owned quite a few Asian designed cars, the car I am thinking of is European-made. ');
 } else {
   userPoints++;
-  alert('You got it!! I am a sucker for all cars, but the one I am thinkin of is not built that far east.');
+  alert('You on the right path!! I\'m definitely thinking of a European-made car. ');
 } alert('You have ' + userPoints + ' points.');
 
-var answer4 = prompt('So know that you know we are talking about European made vehicles. Do you think my dream car is German made? Yes or No.').toLowerCase();
+var answer4 = prompt('Now that you know we are talking about European vehicles, do you think it is German-made? Yes or No.').toLowerCase();
 console.log(answer4);
-if( answer4 === 'yes' || answer4 === 'Y') {
-  alert(' “Ja” you are on the right track, I love the design and build quality the Germans put into their vehicles, but only one company makes “The Ultimate Driving Machine” that I am thinking of. ');
+if( answer4 === 'yes' || answer4 === 'y') {
+  alert('“Ja” you are on the right track, I love the design and build quality the Germans put into their vehicles, but only one company makes “The Ultimate Driving Machine” that I am thinking of. ');
   userPoints++;
 } else {
-  alert(' “Nein” While the Italians and the Brits make amazing cars, this German company is known for creating “The Ultimate Driving Machine” ');
+  alert('“Nein” While the Italians and the Brits make amazing cars, the German car I am thinking of is known as, “The Ultimate Driving Machine”. ');
 } alert('You have ' + userPoints + ' points.');
 
-var answer5 = prompt('Alright ' + user + ', do you think the car in question is a BMW? Yes or No. ').toLowerCase();
+var answer5 = prompt('Alright ' + user + ' last question, do you think the car in question is a BMW? Yes or No. ').toLowerCase();
 console.log(answer5);
-if( answer5 === 'yes' || answer5 === 'Y') {
-  alert('You got it!!! The car in I wish to own soon is the BMW E46 M3 “The Ultimate Driving Machine” ');
+if( answer5 === 'yes' || answer5 === 'y') {
+  alert('You got it!!! The car in I wish to own some day is the BMW E46 M3 “The Ultimate Driving Machine” ');
   userPoints++;
 } else {
-  alert('While I do like Porsche, and Mercedes, BMW\’s are known as “The Ultimate Driving Machine” and the BMW E46 M3 is exactly that!!! ');
+  alert('While I do like Feraris, and Lamborghini, the car I am thinking of is the BMW E46 M3!!! ');
 } alert('You have ' + userPoints + ' points.');
