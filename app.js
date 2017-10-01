40d08faba54238c94ad6366537c720f0761bc05d
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2017-09-30 18:00:13 -0700

    end of day for lab3

--- app.js
+++ app.js
@@ -6,50 +6,79 @@ console.log('Hello');
 alert('Welcome to my guessing game!');
 
 var user = prompt('What is your name?');
+while (!user) {
+  user = prompt('Before we can move on my game, I need to know your name. Please enter below! ');
+}if (user !== 'Brandon') {
+  alert('That\'s a nice name ;) thank\'s for entering it. ');
+}
 console.log('The user\'s name is ' + user);
 alert('Hi ' + user + '! I am so glad you want to play my game.');
 
-var answer = prompt('First question is: Do you think I am a car guy? Please respond yes or no.').toLowerCase();
-console.log(answer);
+//Question 1
+var answer = prompt('First question, do you think I am a car guy? Please respond yes or no. ').toLowerCase();
+console.log(user + ' answered ' + answer + ' for question 1. ');
 if(answer === 'yes' || answer === 'y') {
-  alert('You guessd right ' + user + ', I have owned a vehicle from 13 different manufacturers, and owned 20 different cars, they are my "JAM" ');
   userPoints++;
+  alert('You guessd right ' + user + ', I have owned 20 different cars over the past 15 years. \nYou now have ' + userPoints + ' point.');
 } else {
-  alert('Actually, I am a big car guy, I have had over 20 of them from 13 different manufacturers. “Vroom Vroom” ');
-} alert('You have ' + userPoints + ' points.');
+  alert('Actually, I am a big car guy, I have had over 20 of them. “Vroom Vroom” \nYou still have ' + userPoints + ' points.');
+}
 
-var answer2 = prompt('So ' + user + ', now that you know I "Love" to get behind the wheel, let\'s see if you can guess one of my favorite cars that I hope to own. Do you think my favorite car is American made? Please respond Yes or No. ').toLowerCase();
-console.log(answer2);
+//Question 2
+var answer2 = prompt('Ok ' + user + ', let\'s switch gears, do you think I like Spicy food? Please respond yes or no. ').toLowerCase();
+console.log(user + ' answered ' + answer2 + ' for question 2. ');
 if(answer2 === 'yes' || answer2 === 'y') {
-  alert('Not quite, while I do think it would be fun to shred the tires off a Corvette, it\'s not the one I have in mind. ');
-} else {
   userPoints++;
-  alert('That\’s correct ' + user + ', while I do think American cars are great, I am more a foreign car guy. ');
-} alert('You have ' + userPoints + ' points.');
-
-var answer3 = prompt('So know that you know big American muscle isn\'t for me. Do you think it is an Asian built car ' + user + '? Yes or No.').toLowerCase();
-console.log(answer3);
-if( answer3 === 'yes' || answer3 === 'y') {
-  alert('While I have owned quite a few Asian designed cars, the car I am thinking of is European-made. ');
+  alert('That\’s correct ' + user + ', I love the "HOT" stuff. \nYou now have ' + userPoints + ' point(s).');
 } else {
-  userPoints++;
-  alert('You on the right path!! I\'m definitely thinking of a European-made car. ');
-} alert('You have ' + userPoints + ' points.');
+  alert('Actually, I do love spicy food, the spicier the better I say. \nYou still have ' + userPoints + ' point(s).');
+}
 
-var answer4 = prompt('Now that you know we are talking about European vehicles, do you think it is German-made? Yes or No.').toLowerCase();
-console.log(answer4);
-if( answer4 === 'yes' || answer4 === 'y') {
-  alert('“Ja” you are on the right track, I love the design and build quality the Germans put into their vehicles, but only one company makes “The Ultimate Driving Machine” that I am thinking of. ');
+//Question 3
+var answer3 = prompt('Do you think I am a fan of scary movies ' + user + '? Please respond yes or no. ').toLowerCase();
+console.log(user + ' answered ' + answer3 + ' for question 3. ');
+if( answer3 === 'no' || answer3 === 'n') {
   userPoints++;
+  alert('You got it!! I am not a big horror genre fan. \nYou now have ' + userPoints + ' point(s).');
 } else {
-  alert('“Nein” While the Italians and the Brits make amazing cars, the German car I am thinking of is known as, “The Ultimate Driving Machine”. ');
-} alert('You have ' + userPoints + ' points.');
+  alert('Actually I am not a fan, while I love all movies, the Scary ones I tend to skip. \nYou still have ' + userPoints + ' point(s).');
+}
 
-var answer5 = prompt('Alright ' + user + ' last question, do you think the car in question is a BMW? Yes or No. ').toLowerCase();
-console.log(answer5);
+//Question 4
+var answer4 = prompt('Ok ' + user + ', am I originally from the west coast? Please respond yes or no.').toLowerCase();
+console.log(user + ' answered ' + answer4 + ' for question 4. ');
+if( answer4 === 'no' || answer4 === 'n') {
+  userPoints++;
+  alert('That\'s correct ' + user + ', I moved to Seattle about 7 years ago from North Carolina.  \nYou now have ' + userPoints + ' point(s).');
+} else {
+  alert('While I do consider Seattle my forever home, I am an east coast transplant. \nYou still have ' + userPoints + ' point(s).');
+}
+
+//Question 5
+var answer5 = prompt('Alright ' + user + ', do you think I have any kids? Please respond yes or no. ').toLowerCase();
+console.log(user + ' answered ' + answer5 + ' for question 5. ');
 if( answer5 === 'yes' || answer5 === 'y') {
-  alert('You got it!!! The car in I wish to own some day is the BMW E46 M3 “The Ultimate Driving Machine” ');
   userPoints++;
+  alert('You got it!!! I have a wonderful 4 year old daughter that keeps me really busy. \nYou now have ' + userPoints + ' point(s) for my game, thanks for playing ' + user + '!' );
 } else {
-  alert('While I do like Feraris, and Lamborghini, the car I am thinking of is the BMW E46 M3!!! ');
-} alert('You have ' + userPoints + ' points.');
+  alert('I do in fact have a wonderful 4 year old daughter that keeps me really busy these days.  \nYou still ' + userPoints + ' point(s) for my game, ' + user + '!');
+}
+
+//Question6
+for (var i = 0; i <= 34; i++) {
+  var myAge = parseInt(prompt('Can you guess how old I am? '));
+  console.log('myAge is; ' + myAge);
+  if (myAge < 34) {
+    alert ('I\'m older than that... ');
+  } else if (myAge > 34) {
+    alert ('I am not that old... ');
+  } else if (myAge === 34) {
+    alert ('Yay you got it!');
+    correctCounter += 1;
+  } else {
+    alert('Did you take a guess? ');
+  }
+  if (i >= 34) {
+    alert('Getting closer to 40 :\()');
+  }
+}
